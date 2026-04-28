e17197e6188ea004df7bfe9935db9b192517bfe7
AnkitVS0702 <ankitshiksharthi@gmail.com>
2026-04-28 21:22:52 +0530

    Made background changer project

---
+++ bg_changer/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+// import './App.css'
+
+function App() {
+
+  let [color, setColor] = useState("black")
+
+  function handleColor(color) {
+    return (
+      () => {
+        setColor(color)
+      }
+    )
+  }
+
+  const arr = ["red", "cyan", "green", "yellow"]
+  const buttonList = []
+  for (const item of arr) {
+    buttonList.push(
+      <button id={item} style={{ backgroundColor: item }} className="px-4 py-2" onClick={() => {return setColor(item) }} >
+        {item}
+      </button>
+    );
+  }
+
+  return (
+    <>
+      <div style={{
+        backgroundColor: color,
+        minHeight: '100vh',
+        width: '100%',
+        color: 'white',
+        margin: 0,
+        padding: 0
+      }}>
+        <div className="flex gap-2">
+          {buttonList} {/* Inject the pre-built array here */}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
